d99831f24092cb68ed622b6122e1799906e247cc
chengwen.zheng <chengwenzheng1222@gmail.com>
2022-01-26 21:44:25 +0800

    feat: first type

---
+++ easy/first/index.ts
@@ -0,0 +1,11 @@
+type First<T extends any[]> = T extends [] ? never : T[0];
+
+// infer 的妙用
+// type First<T extends any[]> = T extends [infer P, ...infer R] ? P : never;
+
+
+type arr1 = ["a", "b", "c"];
+type arr2 = [3, 2, 1];
+
+type head1 = First<arr1>; // expected to be 'a'
+type head2 = First<arr2>; // expected to be 3
