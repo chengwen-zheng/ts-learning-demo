5a0f4ffeea6ec4f69719bba6614c8f28d8043622
chengwen.zheng <chengwenzheng1222@gmail.com>
2022-02-22 18:14:15 +0800

    feat: MyExclude

---
+++ README.md
@@ -0,0 +1,1 @@
+仓库复刻 https://github.com/type-challenges/type-challenges/blob/master/README.zh-CN.md
---
+++ easy/exclude/index.ts
@@ -0,0 +1,18 @@
+// Exclude<T, U> 表示从 T 中找出 U 中没有的元素, 换种更加贴近语义的说法其实就是从T 中排除 U
+interface Todo {
+    title: string
+    description: string
+    completed: boolean
+}
+
+interface T1 {
+    title: string;
+}
+
+type MyExclude<T, K> = T extends K ? never : T;
+type T0 = MyExclude<"a" | "b" | "c", "a">;
+type T2 = MyExclude<T1, Todo>;
+
+const a:T2 = {
+    title:'3'
+}
